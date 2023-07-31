6d3de1a7f7b7058870b112d753d0920241dbf777
Nesith Perera <131621142+Nesithp-Perera@users.noreply.github.com>
2023-07-31 15:11:29 +0530

    Add files via upload

---
+++ Lab 6-2023.07.07/Q1/Arrayc.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Arrayc
+    {
+        public int[] CreateArray(int size)
+        {
+            return new int[size];
+        }
+
+        public void PopulateArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Value {i + 1}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public int[] VectorSum(int[] array1, int[] array2)
+        {
+            int size = Math.Min(array1.Length, array2.Length);
+            int[] result = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = array1[i] + array2[i];
+            }
+
+            return result;
+        }
+
+        public int[] VectorProduct(int[] array1, int[] array2)
+        {
+            int size = Math.Min(array1.Length, array2.Length);
+            int[] result = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = array1[i] * array2[i];
+            }
+
+            return result;
+        }
+
+        public int ScalarSum(int[] array1, int[] array2)
+        {
+            int size = Math.Min(array1.Length, array2.Length);
+            int sum = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                sum += array1[i] + array2[i];
+            }
+
+            return sum;
+        }
+
+        public int ScalarProduct(int[] array1, int[] array2)
+        {
+            int size = Math.Min(array1.Length, array2.Length);
+            int product = 1;
+
+            for (int i = 0; i < size; i++)
+            {
+                product *= array1[i] * array2[i];
+            }
+
+            return product;
+        }
+
+        public void DisplayArray(int[] array)
+        {
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lab 6-2023.07.07/Q2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.PrintAnimal();
+
+            Dog dog = new Dog();
+            dog.PrintAnimal();
+            dog.PrintDog();
+            Console.ReadKey();
+            
+        }
+    }
+    
+}
